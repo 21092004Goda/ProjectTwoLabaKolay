d89d138aeae764723187ae5829e30b0c82faad0b
21092004Goda <96621375+21092004Goda@users.noreply.github.com>
2022-09-30 21:59:18 +0300

    Create OPthrie.c

---
+++ OPthrie.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int number_8;
+    printf("Number_8:");
+    scanf("%o", &number_8);
+
+    printf("Number_16 === %X\n", number_8);
+
+    printf("Number << 2: %o\n", number_8 << 2);
+
+    printf("%o\n", ~number_8);
+
+    int num;
+    scanf("%o", &num);
+    printf("%o", num & number_8);
+
+    return 0;
+}
